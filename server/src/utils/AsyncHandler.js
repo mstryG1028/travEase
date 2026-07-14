ce8ea31f83d946605656d82edaefea75d58aeb41
deepak mistry <mistrydeepak1028@gmail.com>
2026-07-15 00:06:08 +0530

    import fix AsyncHandler

--- server/src/utils/AsyncHandler.js
+++ server/src/utils/AsyncHandler.js
@@ -3,8 +3,7 @@
 const AsyncHandler = (requestHandler) => {
   return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => {
-      console.error(err);        // Add this
-      console.error(err.stack);  // Add this
+     
       next(err);
     });
   };
