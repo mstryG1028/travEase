cbcc8a1559a74ec3c9b42e10830f30c0fdfc4720
deepak mistry <mistrydeepak1028@gmail.com>
2026-07-16 00:05:28 +0530

    Fixed sessionOptions for production

--- server/src/utils/cookieOptions.js
+++ server/src/utils/cookieOptions.js
@@ -1,8 +1,17 @@
+// const cookieOptions = {
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === "production",
+//     sameSite: "lax",
+//     maxAge: 10 * 24 * 60 * 60 * 1000
+// };
+
+// export default cookieOptions;
+
 const cookieOptions = {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
-    maxAge: 10 * 24 * 60 * 60 * 1000
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  maxAge: 10 * 24 * 60 * 60 * 1000,
 };
 
 export default cookieOptions;
