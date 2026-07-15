c6c1e3d281bd70e9fdd1169538a5cd1bf9ccb636
Deepak  Mistry <145891697+mstryG1028@users.noreply.github.com>
2026-07-15 21:01:09 +0530

    Fix baseURL syntax in axios configuration

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "../utils/constants";
 
 const api = axios.create({
-  baseURL: " import.meta.env.VITE_API_URL",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // IMPORTANT if using cookies
 });
 
