fe74b944617ec1f0b2af1a4d091290c99a9e5530
Deepak  Mistry <145891697+mstryG1028@users.noreply.github.com>
2026-07-15 20:56:55 +0530

    Update API base URL to use environment variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { API_URL } from "../utils/constants";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: " import.meta.env.VITE_API_URL",
   withCredentials: true, // IMPORTANT if using cookies
 });
 
