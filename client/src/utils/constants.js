9647682056c391ea4c14431caf45099f357e76b6
Deepak  Mistry <145891697+mstryG1028@users.noreply.github.com>
2026-07-15 21:21:38 +0530

    Update API_URL to use environment variable

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const APP_NAME = "TravEase";
 
-export const API_URL = "http://localhost:8000/api/v1";
+export const API_URL = import.meta.env.VITE_API_URL";
 
 export const OWNER = "owner";
 
